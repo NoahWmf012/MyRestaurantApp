8c77985325db4d0813fe2974946ae824f7248f24
NoahWmf012 <wumanfung1@gmail.com>
2025-10-23 14:18:14 -0400

    Add new fire icon

---
+++ src/components/section/RestaurantRank.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RestaurantRank() {
+    return (
+        <div>RestaurantRank</div>
+    )
+}
+
+export default RestaurantRank
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,22 +1,42 @@
 import NewDishes from "../../components/section/NewDishes"
 import OrderAgain from "../../components/section/OrderAgain"
+import RestaurantRank from "../../components/section/RestaurantRank"
 import Reviews from "../../components/section/Reviews"
 import WhatsHot from "../../components/section/WhatsHot"
 
 function HomePage() {
     return (
         <div className="home-page-container">
-            {/* 'What's Hot' section */}
-            <WhatsHot />
+            <main>
+                {/* 'What's Hot' section */}
+                <section className="mb-4">
+                    <WhatsHot />
+                </section>
 
-            {/* 'Order Again' section */}
-            <OrderAgain />
+                {/* Ranks of restaurants */}
+                <section className="mb-4">
+                    <RestaurantRank />
+                </section>
 
-            {/* 'New Dishes' section */}
-            <NewDishes />
+                {/* 'Order Again' section */}
+                <section className="mb-4">
+                    <OrderAgain />
+                </section>
 
-            {/* 'Reviews' section */}
-            <Reviews />
+                {/* 'New Dishes' section */}
+                <section className="mb-4">
+                    <NewDishes />
+                </section>
+
+                {/* 'Reviews' section */}
+                <section className="mb-4">
+                    <Reviews />
+                </section>
+            </main>
+
+            <aside>
+                {/* Ads */}
+            </aside>
         </div>
     )
 }
