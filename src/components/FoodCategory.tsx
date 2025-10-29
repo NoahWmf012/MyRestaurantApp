0707e257cc2d1b66d3934bc8d60df10fff55cf6c
NoahWmf012 <wumanfung1@gmail.com>
2025-10-29 13:04:12 -0400

    Add FoodCategory click navigate

--- src/components/FoodCategory.tsx
+++ src/components/FoodCategory.tsx
@@ -1,13 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import { FOOD_CATEGORIES } from "../constants/foodCategoryList"
 import "./FoodCategory.style.scss"
 
 function FoodCategory() {
+    const navigate = useNavigate();
+
+    const handleCategoryClick = (keyword: string) => {
+        navigate(`/search?query=${encodeURIComponent(keyword)}`);
+    };
+
     return (
         <div className="food-category-container">
             {FOOD_CATEGORIES.map((category) => (
                 <div
                     key={category.name}
                     className="food-category-item"
+                    onClick={() => handleCategoryClick(category.name)}
                 >
                     <img
                         alt={`${category.name} category`}
