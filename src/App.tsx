5c033c8c4fda359c8bf058b816b7c8544007b764
NoahWmf012 <wumanfung1@gmail.com>
2025-11-25 11:57:49 -0500

    Repair Bootstrap problem

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './Router';
 
 //styles
-import './App.css';
+import './App.css'
+import './App.style.scss';
 import './style/theme.scss';
 import 'react-multi-carousel/lib/styles.css'
 import './style/base/baseComponent.scss'
